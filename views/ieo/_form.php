4551653dcaa9403eaad0f6c88d23e60e6d89ef8f
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2019-03-13 21:16:36 -0500

    ajustes para editar

--- views/ieo/_form.php
+++ views/ieo/_form.php
@@ -1,3 +1,5 @@
+
+
 <?php
 
 use yii\helpers\Html;
@@ -12,7 +14,116 @@ $this->registerCssFile("@web/css/modal.css", ['depends' => [\yii\bootstrap\Boots
 $this->registerJs( file_get_contents( '../web/js/ieo.js' ) );
 
 $idTipoInforme = (isset($_GET['idTipoInforme'])) ?  $_GET['idTipoInforme'] :  $model->id_tipo_informe;
+
+
 ?>
+<script>
+	idTipoInforme = <?php echo $idTipoInforme; ?>;
+</script>
+
+<?php 
+
+
+//triger para calcular los totales de estudiantes y docentes
+if( strpos($_GET['r'], 'update') > -1)
+{
+	echo "	
+	<script> 
+	
+
+    
+	$('div[id *= estudiantesieo],[id *= grado]').each(  function( i, val ) 
+	{
+		total =0;
+		
+		//numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
+		ValId = $(this).attr('id');
+		num = ValId.split('-');
+		
+		//cuando se presione un numero se hace la suma
+		$('[id *= estudiantesieo-'+num[1]+'-grado]').each(function( ) 
+		{
+		  total += $(this).val()*1;
+		});
+
+		//se asigna la suma total a la caja de texto correspondiente
+		$('#estudiantesieo-'+num[1]+'-total').val(total);
+		
+	});
+	
+	</script>";
+	
+	
+	if ($idTipoInforme  != 14)
+	{
+		
+	echo "	
+	<script> 
+		$('div[id *= tiposcantidadpoblacion],[id *= tiempo_libre],[id *= edu_derechos],[id *= sexualidad],[id *= ciudadania],[id *= medio_ambiente],[id *= familia],[id *= directivos]').each(  function( i, val ) 
+		{
+			if(idTipoInforme != 14)
+			{
+				total =0;
+				
+				// numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
+				ValId = $(this).attr('id');
+				num = ValId.split('-');
+				
+				// cuando se presione un numero se hace la suma
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-tiempo_libre').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-edu_derechos').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-sexualidad').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-ciudadania').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-medio_ambiente').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-familia').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-directivos').val() * 1 ;
+				
+				// se asigna la suma total a la caja de texto correspondiente
+				$('#tiposcantidadpoblacion-'+num[1]+'-total').val(total);
+			}
+			
+		});
+		
+		
+		</script>";
+	}
+	
+	if ($idTipoInforme  == 14)
+	{
+		
+		echo "	
+		<script> 
+		$('div[id *= tiposcantidadpoblacion],[id *= docentes],[id *= familia],[id *= directivos]').each(  function( i, val ) 
+		{
+			if(idTipoInforme == 14)
+			{
+				total =0;
+				
+				//numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
+				ValId = $(this).attr('id');
+				num = ValId.split('-');
+				
+				//cuando se presione un numero se hace la suma
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-docentes').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-familia').val() * 1 ;
+				total += $('#tiposcantidadpoblacion-'+num[1]+'-directivos').val() * 1 ;
+				
+				//se asigna la suma total a la caja de texto correspondiente
+				$('#tiposcantidadpoblacion-'+num[1]+'-total').val(total);
+			
+			}
+		});
+		
+		</script>";
+	}
+		
+	
+	
+	
+	
+}
+?>
+
 
 <div class="ieo-form">
 
--- web/js/ieo.js
+++ web/js/ieo.js
@@ -7,7 +7,6 @@ $( document ).ready(function()
 	});
 	
 	
-
 });
 	
 //selector para seleccionar solo las celdas de estudiantes
@@ -34,22 +33,24 @@ $('div[id *= estudiantesieo],[id *= grado]').on('keyup mouseup',function(e)
 
 //selector para seleccionar solo las celdas de docentes
 //hace la sumatoria y la asigna a total
-$('div[id *= tiposcantidadpoblacion],[id *= docentes],[id *= psicoorientador],[id *= familia],[id *= directivos]').on('keyup mouseup',function(e) 
+$('div[id *= tiposcantidadpoblacion],[id *= docentes],[id *= familia],[id *= directivos]').on('keyup mouseup',function(e) 
 {
-	total =0;
-	
-	//numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
-	ValId = $(this).attr("id");
-	num = ValId.split("-");
-	
-	//cuando se presione un numero se hace la suma
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-docentes").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-psicoorientador").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-familia").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-directivos").val() * 1 ;
-	
-	//se asigna la suma total a la caja de texto correspondiente
-	$("#tiposcantidadpoblacion-"+num[1]+"-total").val(total);
+	if(idTipoInforme == 14)
+	{
+		total =0;
+		
+		//numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
+		ValId = $(this).attr("id");
+		num = ValId.split("-");
+		
+		//cuando se presione un numero se hace la suma
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-docentes").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-familia").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-directivos").val() * 1 ;
+		
+		//se asigna la suma total a la caja de texto correspondiente
+		$("#tiposcantidadpoblacion-"+num[1]+"-total").val(total+"");
+	}
 
 });
 
@@ -58,23 +59,25 @@ $('div[id *= tiposcantidadpoblacion],[id *= docentes],[id *= psicoorientador],[i
 //hace la sumatoria y la asigna a total
 $('div[id *= tiposcantidadpoblacion],[id *= tiempo_libre],[id *= edu_derechos],[id *= sexualidad],[id *= ciudadania],[id *= medio_ambiente],[id *= familia],[id *= directivos]').on('keyup mouseup',function(e) 
 {
-	total =0;
-	
-	//numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
-	ValId = $(this).attr("id");
-	num = ValId.split("-");
-	
-	//cuando se presione un numero se hace la suma
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-tiempo_libre").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-edu_derechos").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-sexualidad").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-ciudadania").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-medio_ambiente").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-familia").val() * 1 ;
-	total += $("#tiposcantidadpoblacion-"+num[1]+"-directivos").val() * 1 ;
-	
-	//se asigna la suma total a la caja de texto correspondiente
-	$("#tiposcantidadpoblacion-"+num[1]+"-total").val(total);
-
+	if(idTipoInforme != 14)
+	{
+		total =0;
+		
+		// numero del que identifica a la caja de texto Estudiantes Gra para saber a que total debe sumar
+		ValId = $(this).attr("id");
+		num = ValId.split("-");
+		
+		// cuando se presione un numero se hace la suma
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-tiempo_libre").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-edu_derechos").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-sexualidad").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-ciudadania").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-medio_ambiente").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-familia").val() * 1 ;
+		total += $("#tiposcantidadpoblacion-"+num[1]+"-directivos").val() * 1 ;
+		
+		// se asigna la suma total a la caja de texto correspondiente
+		$("#tiposcantidadpoblacion-"+num[1]+"-total").val(total);
+	}
 });
 
