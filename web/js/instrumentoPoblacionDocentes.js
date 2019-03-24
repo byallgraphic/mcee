cd62e7fbee1b4f39b42819094d8b240386070e60
Edwin Molina <edwin.molina@byallgraphic.com>
2019-03-24 09:27:53 -0500

    Se deja en español el datatable en Diario de campo docentes y estudiantes

--- web/js/instrumentoPoblacionDocentes.js
+++ web/js/instrumentoPoblacionDocentes.js
@@ -1,4 +1,29 @@
+// $.fn.dataTable.defaults.oLanguage.sSearch = "Buscar";
 
+// {
+    // "sProcessing":     "Procesando...",
+    // "sLengthMenu":     "Mostrar _MENU_ registros",
+    // "sZeroRecords":    "No se encontraron resultados",
+    // "sEmptyTable":     "Ningún dato disponible en esta tabla",
+    // "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+    // "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+    // "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+    // "sInfoPostFix":    "",
+    // "sSearch":         "Buscar:",
+    // "sUrl":            "",
+    // "sInfoThousands":  ",",
+    // "sLoadingRecords": "Cargando...",
+    // "oPaginate": {
+        // "sFirst":    "Primero",
+        // "sLast":     "Último",
+        // "sNext":     "Siguiente",
+        // "sPrevious": "Anterior"
+    // },
+    // "oAria": {
+        // "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        // "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+    // }
+// }
 
 $( document ).ready(function(){
 	
@@ -511,7 +536,34 @@ $( document ).ready(function(){
 						calcularTotal();
 					});
 					
-					$( "#tbInfo" ).dataTable();
+					$( "#tbInfo" ).dataTable({
+						
+						language: 	{
+										"sProcessing":     "Procesando...",
+										"sLengthMenu":     "Mostrar _MENU_ registros",
+										"sZeroRecords":    "No se encontraron resultados",
+										"sEmptyTable":     "Ningún dato disponible en esta tabla",
+										"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+										"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+										"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+										"sInfoPostFix":    "",
+										"sSearch":         "Buscar:",
+										"sUrl":            "",
+										"sInfoThousands":  ",",
+										"sLoadingRecords": "Cargando...",
+										"oPaginate": {
+											"sFirst":    "Primero",
+											"sLast":     "Último",
+											"sNext":     "Siguiente",
+											"sPrevious": "Anterior"
+										},
+										"oAria": {
+											"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+											"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+										}
+									}
+						
+					});
 
 				},
 			);
