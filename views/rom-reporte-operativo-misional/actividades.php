f95fdf4e88eb8cfa3b3112de8df7bf6911e8c395
Edwin Molina <edwin.molina@byallgraphic.com>
2019-04-04 22:23:48 -0500

    Las actividades se ponen en tab

--- views/rom-reporte-operativo-misional/actividades.php
+++ views/rom-reporte-operativo-misional/actividades.php
@@ -5,14 +5,24 @@ use app\models\IsaRomActividades;
 use yii\helpers\ArrayHelper;
 use yii\bootstrap\Collapse;
 
+use yii\helpers\Html;
 // $model = new IsaRomActividades();
+
+$this->registerJs("
+	$( document ).on('click', 'li', function() { 
+		var cont = $( this ).attr('href');
+		$( '[id^=content][id!='+cont.substr(1)+']', $(this).parent().parent() ).css({display:'none'}); 
+		$( $( this ).attr('href'), $(this).parent().parent() ).toggle();  
+		console.log( $( '[id^=content]', $(this).parent().parent() ) )
+	});
+");
 
 $actividades= IsaRomActividades::find()->where( "estado=1 and id_rom_procesos=$idProceso" )->all();
 $actividades = ArrayHelper::map($actividades,'id','descripcion');
        
 	foreach( $actividades as $idActividad => $v )
 	{
-	 
+		$labels[] = $v;
 		$items[] = 	[
 						'label' 		=>  $v,
 						'content' 		=>  $this->render( 'formulario', 
@@ -25,14 +35,32 @@ $actividades = ArrayHelper::map($actividades,'id','descripcion');
 														] 
 											),
 						'contentOptions'=> []
-					];			
+					];	
+		
 		
 	}
 	
-
+	// foreach( $labels as $key => $item ){
+		
+		echo Html::ul( $labels, ['item' => function($item, $index) {
+						return Html::tag(
+							'li',
+							$item,
+							['class' => 'post' , 'href' => '#content-'.$index, 'name' => 'actividad-'.$index ]
+						);
+					},
+					'class' => 'tabs',
+					] 
+				);
+	// }
+	
+	foreach( $items as $key => $item ){
+		
+		echo Html::tag( 'div', $item['content'], [ 'style' => 'display:none', 'id' => 'content-'.$key ] );
+	}
 				
 
 
-echo Collapse::widget([
-    'items' => $items, 
-]);
+// echo Collapse::widget([
+    // 'items' => $items, 
+// ]);
