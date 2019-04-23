abba763cd422e5f6c897e16db8a707263f92bbd7
vivianaRodas <36738664+vivianaRodas@users.noreply.github.com>
2019-04-22 19:53:33 -0500

    actividades bootstrap formulario 5 -1

--- views/isa-iniciacion-sensibilizacion-artistica-consolidado/actividades.php
+++ views/isa-iniciacion-sensibilizacion-artistica-consolidado/actividades.php
@@ -13,57 +13,80 @@ use dosamigos\datepicker\DatePicker;
 
     <?= Html::activeHiddenInput($actividad, '['.$index.']id') ?>
 	
-    <?= $form->field($actividad, '['.$index.']total_sesiones_realizadas')->textInput() ?>
-
-    <?= $form->field($actividad, '['.$index.']avance_por_mes')->textInput([ 'data-porcentaje' => $index ]) ?>
-
-    <?= $form->field($actividad, '['.$index.']total_sesiones_aplazadas')->textInput() ?>
-
-    <?= $form->field($actividad, '['.$index.']total_sesiones_canceladas')->textInput() ?>
+	<div class="row">
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']total_sesiones_realizadas')->textInput(['readonly' => true]) ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">&nbsp;&nbsp;<?= $form->field($actividad, '['.$index.']avance_por_mes')->textInput(['readonly' => true, 'data-porcentaje' => $index ]) ?> </div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']total_sesiones_aplazadas')->textInput(['readonly' => true]) ?> </div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']total_sesiones_canceladas')->textInput(['readonly' => true]) ?> </div>
+	</div>
+  
 	
 	<h3 style='background-color:#ccc;padding:5px;'><?="Número de participantes por actividad comunitaria por sede educativa." ?></h3>
 
-    <?= $form->field($actividad, '['.$index.']vecinos')->textInput([ "data-participante" => $index ]) ?>
-
-    <?= $form->field($actividad, '['.$index.']lideres_comunitarios')->textInput([ "data-participante" => $index ]) ?>
-
-    <?= $form->field($actividad, '['.$index.']empresarios_comerciantes_microempresas')->textInput([ "data-participante" => $index ]) ?>
-
-    <?= $form->field($actividad, '['.$index.']representantes_organizaciones_locales')->textInput([ "data-participante" => $index ]) ?>
-
-    <?= $form->field($actividad, '['.$index.']asociaciones_grupos_comunitarios')->textInput([ "data-participante" => $index ]) ?>
-
-    <?= $form->field($actividad, '['.$index.']otros_actores')->textInput([ "data-participante" => $index ]) ?>
+	<div class="row">
+	  <div class="col-md-4"><?= $form->field($actividad, '['.$index.']vecinos')->textInput([ "data-participante" => $index ]) ?></div>
+	  <div class="col-md-4"><?= $form->field($actividad, '['.$index.']lideres_comunitarios')->textInput([ "data-participante" => $index ]) ?></div>
+	  <div class="col-md-4"><?= $form->field($actividad, '['.$index.']empresarios_comerciantes_microempresas')->textInput([ "data-participante" => $index ]) ?></div>
+	</div>
 	
-	<div class="form-group">
+	<div class="row">
+	  <div class="col-md-4"><?= $form->field($actividad, '['.$index.']representantes_organizaciones_locales')->textInput([ "data-participante" => $index ]) ?></div>
+	  <div class="col-md-4"><?= $form->field($actividad, '['.$index.']asociaciones_grupos_comunitarios')->textInput([ "data-participante" => $index ]) ?></div>
+	  <div class="col-md-4">&nbsp;<?= $form->field($actividad, '['.$index.']otros_actores')->textInput([ "data-participante" => $index ]) ?></div>
+	</div>
 	
-		<?= Html::label( "Total de Participantes en la actividad." ) ?>
+	<div class="row">
+	  <div class="col-md-4"><?= Html::label( "Total de Participantes en la actividad." ) ?>
 		
 		<?= Html::textInput( $index."-total_participantes", "", [ 
 					"class" 		=> "form-control", 
 					"style" 		=> "background-color:#eee",
 					"disabled" 		=> true, 
 					"id" 			=> $index."-total_participantes", 
-				] ) ?>
+				] ) ?></div>
+	  <div class="col-md-4"></div>
+	  <div class="col-md-4"></div>
+	</div>
+	<div class="form-group">
+	
+		
 		
 	</div>
 
 	<h3 style='background-color:#ccc;padding:5px;'><?="Evidencias (Indique la cantidad y destino de evidencias que resultaron de la actividad, tales  como fotografías, videos, actas, trabajos de los participantes, etc )" ?></h3>
 	
-	<?= $form->field($actividad, '['.$index.']actas')->textInput() ?>
+	<div class="row">
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">&nbsp;<?= $form->field($actividad, '['.$index.']actas')->textInput() ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">&nbsp;<?= $form->field($actividad, '['.$index.']reportes')->textInput() ?> </div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">&nbsp;<?= $form->field($actividad, '['.$index.']listados')->textInput() ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']plan_trabajo')->textInput() ?></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']formato_seguimiento')->textInput() ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']formato_evaluacion')->textInput() ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">&nbsp;<?= $form->field($actividad, '['.$index.']fotografias')->textInput() ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">&nbsp;<?= $form->field($actividad, '['.$index.']videos')->textInput() ?></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><?= $form->field($actividad, '['.$index.']otros_productos')->textInput() ?></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"></div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"></div>
+	</div>
+    
 
-    <?= $form->field($actividad, '['.$index.']reportes')->textInput() ?>
+    
 
-    <?= $form->field($actividad, '['.$index.']listados')->textInput() ?>
+    
 
-    <?= $form->field($actividad, '['.$index.']plan_trabajo')->textInput() ?>
+    
 
-    <?= $form->field($actividad, '['.$index.']formato_seguimiento')->textInput() ?>
+    
 
-    <?= $form->field($actividad, '['.$index.']formato_evaluacion')->textInput() ?>
+    
 
-    <?= $form->field($actividad, '['.$index.']fotografias')->textInput() ?>
+    
 
-    <?= $form->field($actividad, '['.$index.']videos')->textInput() ?>
-
-    <?= $form->field($actividad, '['.$index.']otros_productos')->textInput() ?>
+    
