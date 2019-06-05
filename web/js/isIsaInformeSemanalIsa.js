6f1f7d8cc90bfe6832e49bcae043006c4c7f7d33
Edwin Molina <edwin.molina@byallgraphic.com>
2019-06-04 20:57:08 -0500

    Se agrega filtro por fechas

--- web/js/isIsaInformeSemanalIsa.js
+++ web/js/isIsaInformeSemanalIsa.js
@@ -1,11 +1,16 @@
-	$( "#modalContent" ).on( "change", "#isisainformesemanalisa-nro_semana", function(){
+	$( "#modalContent" ).on( "change", "#isisainformesemanalisa-nro_semana,#isisainformesemanalisa-desde,#isisainformesemanalisa-hasta", function(){
 		
-		$.post( "index.php?r=is-isa-informe-semanal-isa/create",
-			{
-				nroSemana : $( this ).val(),
-			},
-			function(data){
-				$( "#modalContent" ).html( data );
-			}
-		);
+		if( $( "#isisainformesemanalisa-desde" ).val() != '' && $( "#isisainformesemanalisa-hasta" ).val() != '' )
+		{
+			$.post( "index.php?r=is-isa-informe-semanal-isa/create",
+				{
+					nroSemana 	: $( "#isisainformesemanalisa-nro_semana" ).val(),
+					fecha_desde : $( "#isisainformesemanalisa-desde" ).val(),
+					fecha_hasta : $( "#isisainformesemanalisa-hasta" ).val(),
+				},
+				function(data){
+					$( "#modalContent" ).html( data );
+				}
+			);
+		}
 	})
