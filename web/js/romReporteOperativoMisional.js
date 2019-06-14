21d8a6931de24fea8d43eefc2a7a1e07c4a5649e
Edwin Molina <edwin.molina@byallgraphic.com>
2019-06-13 20:44:19 -0500

    Evidencias, tipo de población y logros se muestrans solo si se selecciona un encuentro y un estado para el encuentro

--- web/js/romReporteOperativoMisional.js
+++ web/js/romReporteOperativoMisional.js
@@ -1,4 +1,22 @@
+
+	function habilitarEdicionRegistro( index ){	
 	
+		//Si se ha seleccionado una actividad y un estado
+		var val = 	$( "#isaactividadesrom-"+index+"-sesion_actividad" ).val() != '' 
+				 // && $( "#isaactividadesrom-"+index+"-estado_actividad" ).val() != '' 
+				 && $( "#isaactividadesrom-"+index+"-estado_actividad" ).val() == '179';
+		
+		if( val )
+			$( "#datos_rom_"+index ).css({display:''})
+		else
+			$( "#datos_rom_"+index ).css({display:'none'})
+	}
+	
+	
+	/************************************************************************************************
+	 * Aquí valido los campos que son obligatorios
+	 * Para obligar a que sean obligatorios al array messages se le debe agregar el error
+	 ************************************************************************************************/
 	$( "#modalContent" ).on( 'beforeValidateAttribute', function( evt, attribute, messages ){
 		
 		var val = false;
@@ -9,26 +27,66 @@
 		//La opción depende del controlador
 		var opcion = _target[0];
 		
+		var campo = _target[2];
+		
+		var index = _target[1];
 		//El formulario tiene varios campos con ids formados como controlador-consecutivo-nombre_campo
 		//La condición es que es válido solo si por lo menos un campo es diferente a vacio
 		//siempre se valida todos los que empiezan con controlador-consectivo
 		_target = _target[0]+'-'+_target[1];
 		
+		//Si se ha selecciona un encuentro y un estado
+		var hayEncuentroEstado = 	$( "#isaactividadesrom-"+index+"-sesion_actividad" ).val() != '' 
+								 && $( "#isaactividadesrom-"+index+"-estado_actividad" ).val() == '179';
 		
 		switch( opcion ){
 			
 			case 'isatipocantidadpoblacionrom': 
 			
-				$( "[id^="+_target+"]" ).each(function(){
-					if( $.trim( $( this ).val() ) != '' && $( this ).val()*1 >= 0 ){
-						val = true;
-						return false;
-					}
-				});
-				
-				if( !val )
-					messages.push('Debe ingresar al menos una población');
+				if( hayEncuentroEstado ){
 			
+					$( "[id^="+_target+"]" ).each(function(){
+						if( $.trim( $( this ).val() ) != '' && $( this ).val()*1 >= 0 ){
+							val = true;
+							return false;
+						}
+					});
+					
+					if( !val )
+						messages.push('Debe ingresar al menos una población');
+				}
+			
+			break;
+			
+			case 'isaevidenciasrom':
+			
+				//Si entra por aquí los campos son obligatorios
+				if( hayEncuentroEstado ){
+					// var campos = ['actas', 'reportes', 'listados', 'plan_trabajo', 'formato_seguimiento', 'formato_evaluacion', 'fotografias', 'vidoes', 'otros_productos'];
+					
+					if( $( "#"+attribute.id )[0].files.length == 0 ){
+						messages.push('Debe subir al menos un archivo');
+					}
+				}
+			
+			break
+			
+			case 'isaactividadesromxintegrantegrupo':
+				
+				if( hayEncuentroEstado ){
+					
+					//campos obligatorios
+					var campos = ['duracion_sesion', 'logros', 'fortalezas', 'debilidades', 'alternativas', 'retos', 'articulacion', 'evaluacion', 'observaciones_generales', 'alarmas'];
+					
+					//OJO: se debe validar con !== por que la funcion inArray puede devolver un 0
+					if( $.inArray( campo, campos ) !== false ){
+						//Si se encuentra en la lista de campos se obliga
+						if( $.trim( $( "#"+attribute.id ).val() ) == '' ){
+							messages.push('No puede estar vacío');
+						}
+					}
+				}
+				
 			break;
 			
 			default: break;
@@ -115,6 +173,111 @@
 				.load( $(this).attr('value') );
 	});
 	
+	
+	
+	
+	$( '#modal' )
+		.on( 'change', '[id$=estado_actividad]', function(){ 
+			
+			var __target = this.id.split( '-' );
+			__target = __target[1];
+			
+			var frep = $( '#isaactividadesromxintegrantegrupo'+'-'+__target+'-fecha_reprogramacion' );
+			var jus = $( '#isaactividadesromxintegrantegrupo'+'-'+__target+'-justificacion_activiad_no_realizada' );
+			
+			if( $(this).val() == 179 )
+			{
+				frep.attr({disabled:true})
+				frep.attr({readonly:true})
+				
+				jus.attr({readonly:true})
+					.val('No Aplica');
+			}
+			else
+			{
+				frep.attr({disabled:false});
+				frep.attr({readonly:false});
+				
+				jus.attr({readonly:false})
+					.val('')
+				
+			}
+			
+			habilitarEdicionRegistro( __target );
+		});
+		
+	$( '#modal' )
+		.on( 'change', '[id$=sesion_actividad]', function(){ 
+			
+			var __target = this.id.split( '-' );
+			__target = __target[1];
+			
+			__self = this;
+			
+			if( $( __self ).val() == '' )
+			{
+				$( '#nro_equipo-'+__target ).val( '' )
+				$( '#perfiles-'+__target ).val( '' )
+				$( '#docente_orientador-'+__target ).val( '' )
+			}
+			else
+			{	
+				$.post( 'index.php?r=rom-reporte-operativo-misional/consultar-mision', 
+						{ 
+							rom_actividades	: __target, 
+							sesion_actividad: $( __self ).val() ,
+							nro_semana		: $( '#isaactividadesrom-'+ __target + '-nro_semana' ).val() ,
+						}, 
+						function( data ){
+							if( data != '' ){
+								
+								
+								$.get( 'index.php?r=rom-reporte-operativo-misional/update', 
+										{
+											id: data ,
+										}, 
+										function( data ){
+											$( '#modalContent' ).html( data );
+										}
+								);
+								
+								// $( '#modalContent' ).html( data );
+							}
+							else{
+								
+								$.get( 'index.php?r=rom-reporte-operativo-misional/consultar-intervencion-ieo', 
+										{
+											id: $( __self ).val() ,
+										}, 
+										function( data ){
+											console.log( data );
+											if( data ){
+												if( data.equipo_nombre != '' )
+												{
+													$( '#nro_equipo-'+__target ).val( data.equipo_nombre );
+													$( '.nro_equipo-'+__target ).css({display:''});
+												}
+												else
+												{
+													$( '.nro_equipo-'+__target ).css({display:'none'});
+												}
+												
+												$( '#docente_orientador-'+__target ).val( data.docente_orientador )
+												$( '#perfiles-'+__target ).val( data.perfiles )
+											}
+										},
+										'json'
+								);
+								
+								habilitarEdicionRegistro( __target );
+							}
+						}
+				);
+			}
+		});
+		
+		
+		
 	// var timepicker = new TimePicker('duracion_actividad', {
         // lang: 'en',
         // theme: 'dark'
